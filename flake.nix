d68ac2f4ceb11f61a633302e5cfb082cfbcd8da6
Gregory Bednov <gregorbednov@gmail.com>
2026-08-29 08:19:16 +0300

    flake.nix added

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Surface relation extractor for OCCT";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cmake
+          opencascade-occt
+          clang
+        ];
+      };
+      defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation {
+        pname = "OcctSurfacer";
+        version = "1.0.0";
+        src = ./.;
+        nativeBuildInputs = [
+          pkgs.cmake
+          pkgs.opencascade-occt
+        ];
+
+        cmakeFlags = [ "-DBUILD_TESTING=OFF" ];
+      };
+    };
+}
